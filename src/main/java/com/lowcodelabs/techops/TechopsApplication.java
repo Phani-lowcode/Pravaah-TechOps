0c6770487aa9ca3eb8feb649495f82f63b6f2cfc
Phani-lowcode <leelaphanindra@lowcodelabs.in>
2026-04-09 13:16:30 +0530

    Initial commit

---
+++ src/main/java/com/lowcodelabs/techops/TechopsApplication.java
@@ -0,0 +1,13 @@
+package com.lowcodelabs.techops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lowcodelabs/techops/TechopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lowcodelabs.techops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
